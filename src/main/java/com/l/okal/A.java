d38ae36105521ee243a6f84957e6180d07ffb903
Dobby233Liu <papaqi1234567@126.com>
2018-02-17 17:33:59 +0800

    Lol

---
+++ src/main/java/com/l/okal/A.java
@@ -0,0 +1,25 @@
+package com.l.okal;
+
+import android.app.*;
+import android.os.*;
+import android.app.admin.*;
+import android.content.*;
+
+public class A extends Activity 
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    	DevicePolicyManager policyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+    	ComponentName componentName = new ComponentName(this, B.class);
+    	boolean isActive = policyManager.isAdminActive(componentName);
+        if (isActive) {
+        	 policyManager.lockNow();
+        }else{
+			 startActivityForResult(new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName), 0);
+    	}
+	   finish();
+    }
+}
---
+++ src/main/java/com/l/okal/B.java
@@ -0,0 +1,6 @@
+package com.l.okal;
+import android.app.admin.*;
+
+public class B extends DeviceAdminReceiver
+{
+}
