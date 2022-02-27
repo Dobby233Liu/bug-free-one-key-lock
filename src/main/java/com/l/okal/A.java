016195f3870e6af6b7ed28a6397d05a2b45f3b30
Liu Wenyuan <15816141883@163.com>
2022-02-27 13:12:43 +0800

    Update A.java

--- src/main/java/com/l/okal/A.java
+++ src/main/java/com/l/okal/A.java
@@ -12,14 +12,14 @@ public class A extends Activity
     {
         super.onCreate(savedInstanceState);
     	DevicePolicyManager policyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-	ComponentName componentName = new ComponentName(this, android.app.admin.DeviceAdminReceiver.class);
+        ComponentName componentName = new ComponentName(this, android.app.admin.DeviceAdminReceiver.class);
     	boolean isActive = policyManager.isAdminActive(componentName);
         if (isActive) {
-		policyManager.lockNow();
-        }else{
-		startActivityForResult(new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName), 0);
+            policyManager.lockNow();
+        } else {
+            startActivityForResult(new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName), 0);
     	}
-	   android.os.Process.killProcess(android.os.Process.myPid());
+        android.os.Process.killProcess(android.os.Process.myPid());
         finish();
     }
 }
